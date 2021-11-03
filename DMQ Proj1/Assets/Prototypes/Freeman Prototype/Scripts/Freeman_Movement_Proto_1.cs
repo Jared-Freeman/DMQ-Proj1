2e917442ce997f9c49c46046d1b491f4665a7420
Jared-Freeman <j99freeman@gmail.com>
2021-11-02 20:50:06 -0700

    dev materials

--- DMQ Proj1/Assets/Prototypes/Freeman Prototype/Scripts/Freeman_Movement_Proto_1.cs
+++ DMQ Proj1/Assets/Prototypes/Freeman Prototype/Scripts/Freeman_Movement_Proto_1.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
+
+
 public class Freeman_Movement_Proto_1 : MonoBehaviour
 {
+
+    [SerializeField]
+    private float axis_x;
+    [SerializeField]
+    private float axis_y;
+
+    private Rigidbody attached_rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        attached_rb = GetComponent<Rigidbody>();
+        if (attached_rb == null) Debug.LogError("Rigidbody not attached to movement system!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GatherInput();
+        DetermineMovementStates();
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        float horizontal_force = 5f;
+        attached_rb.AddForce(new Vector3(axis_x * horizontal_force, 0, axis_y * horizontal_force));
+    }
+
+    private void DetermineMovementStates()
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void GatherInput()
+    {
+        axis_x = Input.GetAxisRaw("Horizontal");
+        axis_y = Input.GetAxisRaw("Vertical");
     }
 }
