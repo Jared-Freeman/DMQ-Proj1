dbd7fbd6209717232abebb1cd3464692ce974c72
Rycorn <44075236+Rycorn@users.noreply.github.com>
2021-11-18 18:20:41 -0800

    added Ryan stuff

---
+++ DMQ Proj1/Assets/Ryan Assets/Resources/PlayerControls.cs
@@ -0,0 +1,361 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Resources/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""f840efd3-0628-420f-a381-521012eb72b8"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""822c120e-aa06-476b-8b24-3f46fb2e2cc6"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""967d8b77-692d-4291-8190-13516d9a817b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack1"",
+                    ""type"": ""Button"",
+                    ""id"": ""a82f6022-213e-4432-b3bd-f3f958f5bd78"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack2"",
+                    ""type"": ""Button"",
+                    ""id"": ""d54a1cd3-1d00-4736-878c-830882af2b85"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack3"",
+                    ""type"": ""Button"",
+                    ""id"": ""7ffe070d-f219-46a7-99bd-69d86ce998c9"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack4"",
+                    ""type"": ""Button"",
+                    ""id"": ""a71f9a18-a152-4bed-9f11-e023aca4de2b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""ea421c25-4b1a-45ba-ab38-7ef1914f51a7"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d824b3e2-0838-4ea8-af03-6bf8c89b78a3"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""48286497-0f1d-4270-b5f5-7de2ea52b224"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""75f450ac-68a4-4209-90b3-c69f1b05efbb"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8804e3f7-825a-4412-ab3c-9f67728ee130"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""d53b764f-03a5-4fe9-87eb-33151efe535b"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0473e915-6ca1-415f-b426-2e00d5802387"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""80f2e6d3-5a08-4eea-9246-e24f00f1dd37"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e4e3fde3-0672-45c1-81a2-1eed6ee75d0e"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack1"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1ac62940-b2d6-4590-ad63-917a4fe807d3"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack3"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d4ff1678-bcae-4fd2-a9da-21e8bacccf71"",
+                    ""path"": ""<Gamepad>/buttonNorth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack4"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""817c4068-2a21-45f3-880d-54d2c369e165"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""51b869d9-51cc-4194-8251-0f8133bcd35b"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Attack1 = m_Player.FindAction("Attack1", throwIfNotFound: true);
+        m_Player_Attack2 = m_Player.FindAction("Attack2", throwIfNotFound: true);
+        m_Player_Attack3 = m_Player.FindAction("Attack3", throwIfNotFound: true);
+        m_Player_Attack4 = m_Player.FindAction("Attack4", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Attack1;
+    private readonly InputAction m_Player_Attack2;
+    private readonly InputAction m_Player_Attack3;
+    private readonly InputAction m_Player_Attack4;
+    private readonly InputAction m_Player_Dash;
+    public struct PlayerActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PlayerActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Attack1 => m_Wrapper.m_Player_Attack1;
+        public InputAction @Attack2 => m_Wrapper.m_Player_Attack2;
+        public InputAction @Attack3 => m_Wrapper.m_Player_Attack3;
+        public InputAction @Attack4 => m_Wrapper.m_Player_Attack4;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Attack1.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack1;
+                @Attack1.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack1;
+                @Attack1.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack1;
+                @Attack2.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack2;
+                @Attack2.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack2;
+                @Attack2.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack2;
+                @Attack3.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack3;
+                @Attack3.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack3;
+                @Attack3.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack3;
+                @Attack4.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack4;
+                @Attack4.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack4;
+                @Attack4.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack4;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Attack1.started += instance.OnAttack1;
+                @Attack1.performed += instance.OnAttack1;
+                @Attack1.canceled += instance.OnAttack1;
+                @Attack2.started += instance.OnAttack2;
+                @Attack2.performed += instance.OnAttack2;
+                @Attack2.canceled += instance.OnAttack2;
+                @Attack3.started += instance.OnAttack3;
+                @Attack3.performed += instance.OnAttack3;
+                @Attack3.canceled += instance.OnAttack3;
+                @Attack4.started += instance.OnAttack4;
+                @Attack4.performed += instance.OnAttack4;
+                @Attack4.canceled += instance.OnAttack4;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnAttack1(InputAction.CallbackContext context);
+        void OnAttack2(InputAction.CallbackContext context);
+        void OnAttack3(InputAction.CallbackContext context);
+        void OnAttack4(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
+    }
+}
---
+++ DMQ Proj1/Assets/Ryan Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,252 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControler : MonoBehaviour
+{
+    #region Members
+    int resetAttackStatehash = Animator.StringToHash("lastStrike");
+    #region variables
+    [Header("variables")]
+    [Range(.1f, 500f)]
+    [Tooltip("m / sec^2")]
+    public float acceleration = 1;
+
+    [Range(.1f, 500f)]
+    [Tooltip("m / sec^2")]
+    public float damping = 1;
+
+    [Tooltip("Meters / sec")]
+    public float MoveSpd = 1;
+    public float dashMultiplier = 2;
+    public float jumpHeight = 1;
+    public bool dashing = false;
+    #endregion
+
+    #region External components
+    [Space(10)]
+
+    [Header("External components")]
+    public GameObject HorizontalMovementAngleHost;
+    [SerializeField]
+    private Rigidbody RB;
+    
+    private PlayerControls controls;
+    public PlayerInput Input;
+    public Animator anim;
+    #endregion
+
+    #region Movement maps
+    [Space(20)]
+
+    [Header("Movement maps")]
+    //affects horizontal movement velocity. Controlled via input
+    [SerializeField]
+    Vector2 VelocityMap = Vector2.zero; 
+    
+    [SerializeField]
+    Vector2 InputMap = Vector2.zero;
+    
+    [SerializeField]
+    Vector2 DragVector = Vector2.zero;
+    #endregion
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody>();
+        if (HorizontalMovementAngleHost == null)
+        {
+            HorizontalMovementAngleHost = gameObject;
+        }
+        controls = new PlayerControls();
+        InitializePlayer();
+    }
+
+    public void InitializePlayer()
+    {
+        Input.onActionTriggered += Input_onActionTriggered;
+    }
+
+    void Update()
+    {
+        if (new Vector3(RB.velocity.x, 0, RB.velocity.z).sqrMagnitude > MoveSpd * MoveSpd * 4)
+        {
+            Debug.Log("Exceeding");
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).tagHash == resetAttackStatehash)
+        {
+            anim.ResetTrigger("AttackTrigger");
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).tagHash == Animator.StringToHash("dashing"))
+        {
+            anim.ResetTrigger("Dash");
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //if (new Vector3(RB.velocity.x, 0, RB.velocity.z).sqrMagnitude < MoveSpd * MoveSpd * 4)
+        HorizontalMovementInput();
+        UpdateRotation();
+
+        //RB.AddForce(Vector3.down * AddedDownwardForce * RB.mass * Time.fixedDeltaTime, ForceMode.Force);
+    }
+
+    #region Input Handlers
+    public Vector2 desiredVelocity = Vector2.zero;
+    public Vector2 normalizdVelocity = Vector2.zero;
+    public float normalizdVelocityMagnitude = 0;
+    private void HorizontalMovementInput()
+    {
+        Vector3 RetainedVelocity = RB.velocity;
+
+        if (InputMap.sqrMagnitude > 1)
+        {
+            InputMap.Normalize();
+        }
+
+        DragVector = Vector3.zero;
+        //kill all stuff not in the dir we wanna go
+        if (InputMap != Vector2.zero)
+        {
+            DragVector = Vector2.Perpendicular(InputMap);
+        }
+        else
+        {
+            DragVector = -1 * VelocityMap;
+        }
+
+        DragVector.Normalize();
+
+        DragVector *= Vector2.Dot(DragVector, -1 * VelocityMap) * Time.fixedDeltaTime * damping;
+
+        desiredVelocity = InputMap * MoveSpd;
+        if(dashing)
+        {
+            desiredVelocity *= dashMultiplier;
+        }
+
+        float acc = InputMap.magnitude > 0.1f ? acceleration : damping;
+
+        if (VelocityMap.sqrMagnitude > MoveSpd)
+        {
+            //VelocityMap = VelocityMap.normalized * MoveSpd; //max spd clamp
+            acc = damping;
+        }
+
+        VelocityMap.x = Mathf.MoveTowards(VelocityMap.x, desiredVelocity.x, acc );
+        VelocityMap.y = Mathf.MoveTowards(VelocityMap.y, desiredVelocity.y, acc);
+
+        normalizdVelocity = VelocityMap / MoveSpd;
+        normalizdVelocityMagnitude = normalizdVelocity.magnitude;
+        anim.SetFloat("Velocity Z", normalizdVelocityMagnitude);
+        if(VelocityMap.magnitude > 0.1f)
+        {
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+
+        Vector3 VelocityMap3 = new Vector3(VelocityMap.x, 0, VelocityMap.y);
+        //VelocityMap3 += RetainedVelocity;
+
+        RB.AddForce(VelocityMap3 - RB.velocity, ForceMode.VelocityChange);
+    }
+    private void UpdateRotation()
+    {
+        Vector2 movementInput = InputMap.normalized;
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+    private void JumpInput()
+    {
+        //Debug.Log("JumpInput");
+
+        StopCoroutine(JumpStart());
+        StartCoroutine(JumpStart());
+
+        //float JumpSpeed = Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y);
+        //RB.AddForce(JumpSpeed * Vector3.up, ForceMode.VelocityChange);
+
+        //StartCoroutine(JumpNormal(1));
+        //RB.AddForce(HorizontalMovementAngleHost.transform.forward * 5 * RB.mass, ForceMode.Impulse);
+    }
+
+    public void DashReset()
+    {
+        dashing = false;
+    }
+
+    public void DashSet()
+    {
+        dashing = true;
+    }
+    #endregion
+
+    public IEnumerator JumpNormal(float duration)
+    {
+        float StartTime = Time.time;
+        float LastCurTime = Time.time;
+        float CurTime = Time.time;
+        while (Mathf.Abs(CurTime - StartTime) < duration)
+        {
+            LastCurTime = CurTime;
+            CurTime = Time.time;
+            RB.AddForce(HorizontalMovementAngleHost.transform.forward * 50 * RB.mass * (Mathf.Abs(CurTime - LastCurTime) / duration), ForceMode.Force);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+    }
+    public IEnumerator JumpStart()
+    {
+        yield return new WaitForFixedUpdate();
+
+        float JumpSpeed = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+        RB.AddForce(JumpSpeed * Vector3.up, ForceMode.VelocityChange);
+
+    }
+
+    private void Input_onActionTriggered(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.name == controls.Player.Movement.name)
+        {
+            InputMap = ctx.ReadValue<Vector2>();
+        }
+        if (ctx.action.name == controls.Player.Jump.name)
+        {
+            if (ctx.performed)
+            {
+                //JumpInput();
+            }
+        }
+        if (ctx.action.name == controls.Player.Dash.name)
+        {
+            if (ctx.performed && !dashing)
+            {
+                //SprintMovement();
+                anim.SetTrigger("Dash");
+            }
+        }
+        if (ctx.action.name == controls.Player.Attack1.name)
+        {
+            if (ctx.performed)
+            {
+                anim.SetTrigger("AttackTrigger");
+            }
+            
+        }
+    }
+
+}
