1bd9ec966fb3fb0115499262b8a11bcd56454007
jeffgilbertson <jeffgilbertson@nevada.unr.edu>
2021-11-18 17:41:16 -0800

    Added Jeff's assets
    Includes very basic enemy prefab
    -Health, healthbar
    - Enemy can take damage and will be deactivated when health reaches 0

---
+++ DMQ Proj1/Assets/Jeff Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //This class holds stats about the enemies such as health
+
+    public int currentHealth;
+    public int maxHealth = 10;
+    public Healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //For now, press R to do 1 damage to the enemy.
+        //This may be later called by an event manager.
+        if (Input.GetKeyDown(KeyCode.R))
+            TakeDamage(3);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+        if (currentHealth <= 0)
+        {
+            //Enemy is dead
+            Debug.Log("Enemy is dead");
+            gameObject.SetActive(false);
+            healthbar.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Enemy's current health: " + currentHealth);
+            healthbar.SetHealth(currentHealth);
+        }
+    }
+}
