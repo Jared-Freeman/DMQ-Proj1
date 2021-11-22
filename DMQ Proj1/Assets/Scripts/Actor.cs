00599f927ff4a51867e8d3b586dafd4d231e6d13
jeffgilbertson <jeffgilbertson@nevada.unr.edu>
2021-11-21 19:07:00 -0800

    Created generic Actor Scripts
    Added first versions of the Actor, Actor AI, and ActorStat classes.

---
+++ DMQ Proj1/Assets/Scripts/Actor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ActorStats))]
+public class Actor : MonoBehaviour
+{
+
+    #region members
+
+    private static int IdGenerator = 0;
+    
+    readonly public int ID = IdGenerator++;
+
+    public ActorStats stats;
+
+    #endregion
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DMQ Proj1/Assets/Scripts/ActorStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorStats : MonoBehaviour
+{
+
+    #region members
+    
+    public float HpMax = 0;
+    public float EnergyMax = 0;
+
+    
+    public float HpCurrent;
+    public float EnergyCurrent;
+
+    #endregion
+
+    void Start()
+    {
+        HpCurrent = HpMax;
+        EnergyCurrent = EnergyMax;
+    }
+
+}
