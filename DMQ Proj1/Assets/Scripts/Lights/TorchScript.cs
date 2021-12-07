e985f89f77d0be1a7bf097299d57f1da3f5aef2f
Evilash83 <67073295+Evilash83@users.noreply.github.com>
2021-12-07 09:29:11 -0800

    Lights
    Create a light script for torches that can be applied to any torch light source (point light).

---
+++ DMQ Proj1/Assets/Scripts/Lights/TorchScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+    Light torch;
+    float baseIntensity = 2;
+    float baseRange = 3;
+    public float max = 3.0f;
+    public float min = 1.0f;
+    float t = 0;
+    void Start()
+    {
+        torch = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        torch.intensity = baseIntensity + Mathf.Lerp(min, max, t);
+        torch.range = baseRange + Mathf.Lerp(min, max, t);
+        if (t > 1.0)
+        {
+            float temp = max;
+            max = min;
+            min = temp;
+            t = 0.0f;
+        }
+    }
+}
