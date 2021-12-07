333c222f6f8ba93c67bbffe2c91d6265bedb9bad
omarmartinez310 <omarmartinez1488@gmail.com>
2021-12-07 11:02:42 -0800

    ParticleCollison
    Player will interact will with other objects if player hit another object particles  will come out and when particle shoots and interacts with other particles the object will release particles

---
+++ DMQ Proj1/Assets/Script/TargetHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHit : MonoBehaviour{
+
+    [SerializeField] ParticleSystem hit;
+    void OnParticleCollision(GameObject other){
+        Debug.Log($" I'm hit by! {other.gameObject.name}");
+        hit.Play();
+        Destroy(gameObject,2f);
+    }
+}
