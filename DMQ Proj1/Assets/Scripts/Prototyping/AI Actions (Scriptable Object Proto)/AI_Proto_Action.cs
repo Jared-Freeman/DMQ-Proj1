8b2635ff15ccc2654f4b4f10ded8486139951787
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-01-04 14:06:13 -0800

    AI testing rudiments

---
+++ DMQ Proj1/Assets/Scripts/Prototyping/AI Actions (Scriptable Object Proto)/AI_Proto_Action.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AI_Proto_Action", menuName = "ScriptableObjects/AI_Proto_Action/AI_Proto_Action_SO", order = 1)] //May want to remove this from menu
+public class AI_Proto_Action : ScriptableObject
+{
+}
