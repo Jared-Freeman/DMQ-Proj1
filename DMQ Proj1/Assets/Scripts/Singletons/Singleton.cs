510820a00e5fa0246908d3309846a650e4f51756
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-01-13 10:10:03 -0800

    Singleton abstract class

---
+++ DMQ Proj1/Assets/Scripts/Singletons/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance != null)
+            {
+
+            }
+            else
+            {
+                //find instance if it exists
+                m_instance = FindObjectOfType<T>();
+
+                //Create new instance
+                if(m_instance == null)
+                {
+                    GameObject GO = new GameObject();
+
+                    GO.name = typeof(T).Name + " [Singleton]";
+
+                    m_instance = GO.AddComponent<T>();
+                }
+            }
+
+
+            return m_instance;
+        }
+
+    }
+
+    protected virtual void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
