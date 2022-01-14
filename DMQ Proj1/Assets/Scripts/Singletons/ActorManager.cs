cbc621ddd61225c717fec6774592e39152aedc06
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-01-13 16:02:04 -0800

    Team impl, Team Manager singleton impl, Cooldown Utility impl

---
+++ DMQ Proj1/Assets/Scripts/Singletons/ActorManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Maintains a list of all Actors
+/// </summary>
+public class ActorManager : Singleton<ActorManager>
+{
+    //[Header("Tracks a list of all active Actors")]
+
+    public bool FLAG_Debug = false;
+
+    public List<Actor> ActorList { get; } = new List<Actor>();
+
+    #region Initialization
+    protected override void Awake()
+    {
+        base.Awake();
+
+        ////add any actors, if they exist
+        //List<Actor> DanglingActors = FindObjectsOfType<Actor>().ToList();
+        //foreach(Actor a in DanglingActors)
+        //{
+        //    if (FLAG_Debug) Debug.Log("ADDED DANLGING ACTOR: " + a.name);
+        //    ActorList.Add(a);
+        //}
+    }
+
+    private void OnEnable()
+    {
+        EventSubscribe();
+    }
+    private void OnDisable()
+    {
+        EventUnsubscribe();
+    }
+    #endregion
+
+    #region Events
+
+    #region Event Subscriptions
+
+    void EventSubscribe()
+    {
+        Actor.OnActorCreated += AddActorToList;
+        Actor.OnActorDestroyed += RemoveActorFromList;
+    }
+    void EventUnsubscribe()
+    {
+        Actor.OnActorCreated -= AddActorToList;
+        Actor.OnActorDestroyed -= RemoveActorFromList;
+    }
+
+    #endregion
+
+    #region Event Handlers
+
+    private void AddActorToList(object caller, CSEventArgs.ActorEventArgs args)
+    {
+        if (FLAG_Debug) Debug.Log("ADDED <event> ACTOR: " + args._Actor.name);
+        ActorList.Add(args._Actor);
+    }
+    private void RemoveActorFromList(object caller, CSEventArgs.ActorEventArgs args)
+    {
+        if (FLAG_Debug) Debug.Log("REMOVED <event> ACTOR: " + args._Actor.name);
+        ActorList.Remove(args._Actor);
+    }
+
+    #endregion
+
+    #endregion
+
+
+}
