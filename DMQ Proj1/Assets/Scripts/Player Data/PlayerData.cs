8c3ec868bb1ddd0ccf490f952d21f93e91028e00
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-01-21 12:15:32 -0800

    Save data initial impl
    Saving works, but no data is being serialized and stored currently. Need to determine data that should be saved.

---
+++ DMQ Proj1/Assets/Scripts/Player Data/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Container for Player Data for use in-session. Info can be extrapolated and saved afterward
+/// </summary>
+[System.Serializable]
+public class PlayerData_Session
+{
+    public PlayerData_Session(string savePath)
+    {
+        SaveDataPath = savePath;
+    }
+
+    public readonly string SaveDataPath;
+
+    //Data during runtime gets stored here:
+    //character level 
+    //preferences
+    //etc
+}
