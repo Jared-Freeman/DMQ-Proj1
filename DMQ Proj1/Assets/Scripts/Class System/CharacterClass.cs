282e6fb6605818afb67dad066d4c0fced5d29f20
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-01-30 14:46:59 -0800

    Class Rudiments

---
+++ DMQ Proj1/Assets/Scripts/Class System/CharacterClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Preset for a Character Class
+/// </summary>
+public class CharacterClass : ScriptableObject
+{
+    /// <summary>
+    /// Container for the rules this class follows, such as their set of useable stuff (perhaps), base stats, etc.
+    /// </summary>
+    [System.Serializable]
+    public struct ClassInfo
+    {
+
+    }
+
+    public string ClassName;
+    [Multiline]
+    public string DisplayInfo;
+}
