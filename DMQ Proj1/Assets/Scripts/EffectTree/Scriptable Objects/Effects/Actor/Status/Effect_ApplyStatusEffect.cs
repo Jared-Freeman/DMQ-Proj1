15293b46971efea7e5072e8140b9cfa31100c081
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-02-04 15:00:56 -0800

    Impl Apply damage, status effect EffectTree Effects

---
+++ DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Effects/Actor/Status/Effect_ApplyStatusEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ActorSystem.StatusEffect;
+
+namespace EffectTree
+{
+    /// <summary>
+    /// Applies status effect(s) to target actor, if one exists
+    /// </summary>
+    [CreateAssetMenu(fileName = "ASE_", menuName = "Effect Tree/Apply Status Effect", order = 2)]
+    public class Effect_ApplyStatusEffect : Effect_Base
+    {
+        public TargetFilterOptions TargetFilters;
+        /// <summary>
+        /// List of statuses to apply to target
+        /// </summary>
+        public List<SE_StatusEffect_Base> List_StatusEffects;
+
+        public override bool Invoke(ref EffectContext ctx)
+        {
+            Actor actor = ctx.AttackData._TargetGameObject.GetComponent<Actor>();
+
+            //this branch includes a short circuit eval
+            if(actor != null && TargetFilters.TargetIsAllowed(ctx.AttackData._Team, actor) && base.Invoke(ref ctx))
+            {
+                foreach(SE_StatusEffect_Base fx in List_StatusEffects)
+                {
+                    actor.Stats.AddStatusEffect(fx.CreateInstance(actor.gameObject));
+                }
+
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
