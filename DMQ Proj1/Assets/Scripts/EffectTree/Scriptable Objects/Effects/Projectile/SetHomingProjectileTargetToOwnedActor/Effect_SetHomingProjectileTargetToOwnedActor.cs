2d526f35fe736ec481ba9f0d3be0725c20610f54
JeffGilbertson <jeffgilbertson@nevada.unr.edu>
2022-02-05 15:35:22 -0800

    Added Boomerang Weapon/Ability
    Added a weapon that calls an ability which fires a projectile  that returns to the player after it hits something

---
+++ DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Effects/Projectile/SetHomingProjectileTargetToOwnedActor/Effect_SetHomingProjectileTargetToOwnedActor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EffectTree
+{
+    [CreateAssetMenu(fileName = "LP_", menuName = "Effect Tree/Set Homing Target to Owned Actor", order = 1)]
+    public class Effect_SetHomingProjectileTargetToOwnedActor : Effect_Base
+    {
+        public GameObject ProjectilePrefab;
+        public bool UseTeamNoCollideLayer = false;
+
+        public override bool Invoke(ref EffectContext ctx)
+        {
+            if(base.Invoke(ref ctx))
+            {
+                var Projectile = ProjectilePrefab.GetComponent<GenericProjectile>();
+                if (Projectile == null) return false;
+
+                //Projectile.Info.Target = ctx.AttackData._Owner.gameObject;
+
+                var instance = Utils.Projectile.CreateProjectileFromAttackContext(Projectile, ctx.AttackData);
+                if (instance == null) return false;
+
+                instance.Info.Target = ctx.AttackData._Owner.gameObject;
+                if (ctx.AttackData._Team != null)
+                {
+                    if (UseTeamNoCollideLayer)
+                    {
+                        if (!Utils.TransformUtils.ChangeLayerOfGameObjectAndChildren(instance.gameObject, ctx.AttackData._Team.Options.NoCollideLayer))
+                        {
+                            Debug.LogError("Error converting projectile to team layer! Is layer set within proper bounds?");
+                        }
+                    }
+                    else
+                    {
+                        if (!Utils.TransformUtils.ChangeLayerOfGameObjectAndChildren(instance.gameObject, ctx.AttackData._Team.Options.Layer))
+                        {
+                            Debug.LogError("Error converting projectile to team layer! Is layer set within proper bounds?");
+                        }
+                    }
+                }
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ DMQ Proj1/Assets/Scripts/ItemSystem (IS)/IS_Item/Weapon/Subclass/DevBoomerang/Item_Weapon_DevBoomerang.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+using AbilitySystem;
+
+namespace ItemSystem.Weapons
+{
+    public class Item_Weapon_DevBoomerang : Item_WeaponBase
+    {
+        public Item_Weapon_DevBoomerangPreset DevBoomerangPreset;
+
+        protected StateInfo DevBoomerang_Info;
+
+        [System.Serializable]
+        protected struct StateInfo
+        {
+            public AS_Ability_Instance_Base AttachedAbilityInstance;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if(DevBoomerangPreset == null)
+            {
+                Debug.LogError("Preset not set!!");
+                Destroy(gameObject);
+            }
+
+            DevBoomerang_Info.AttachedAbilityInstance = DevBoomerangPreset.Ability.GetInstance(gameObject);
+        }
+
+        public override bool InvokeAttack(AttackContext ctx)
+        {
+            if(base.InvokeAttack(ctx))
+            {
+                EffectTree.EffectContext c = new EffectTree.EffectContext();
+                c.AttackData = ctx;
+
+                if(DevBoomerang_Info.AttachedAbilityInstance)
+                {
+                    DevBoomerang_Info.AttachedAbilityInstance.ExecuteAbility(ref c);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
