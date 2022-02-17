918a23da299e00838e9177f9a57f9fb3aee19520
Evilash83 <67073295+Evilash83@users.noreply.github.com>
2022-02-16 20:09:25 -0800

    Demo URP branch
    New branch for creating a demo using URP

---
+++ DMQ Proj1/Assets/UI Components/HealthBar/HealthBar/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Material healthBar;
+    public float maxHealth;
+    public float curHealth;
+    public float maxHealthTotal;
+    public float healthPercentage;
+
+    private float red = 0.0f;
+    private float green = 1.0f;
+    private float blue = 0.0f;
+    private float albedo = 1.0f;
+    private Color color = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+
+    void Start()
+    {
+        healthBar.SetFloat("_Health", curHealth);
+        healthBar.SetFloat("_CurrentMaxHealth", maxHealth);
+        healthBar.SetFloat("_MaximumHealth", maxHealthTotal);
+        healthBar.SetColor("_Color", color);
+    }
+
+    void Update()
+    {
+        takeDamage();
+    }
+
+    void takeDamage()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            curHealth -= 3;
+            healthBar.SetFloat("_Health", curHealth);
+        }
+        updateHealthColor();
+    }
+
+    void updateHealthColor()
+    {
+        healthPercentage = curHealth / maxHealth;
+        if (healthPercentage >= 0.5f)
+        {
+            red = (1 - healthPercentage) * 2; 
+            green = 1.0f;
+        }
+        else
+        {
+            red = 1.0f;
+            green = 1.0f - ((1.0f - healthPercentage));
+        }
+        color = new Color(red, green, blue, albedo);
+        healthBar.SetColor("_Color", color);
+    }
+}
---
+++ DMQ Proj1/Assets/UI Components/Weapons/UIWeaponSwap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWeaponSwap : MonoBehaviour
+{
+    public GameObject weaponOne;
+    public GameObject weaponTwo;
+    public GameObject rotationPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponOne = GameObject.Find("WeaponOne");
+        weaponTwo = GameObject.Find("WeaponTwo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) ;
+        {
+            weaponOne.transform.RotateAround(rotationPoint.transform.position, Vector3.up,20 * Time.deltaTime);
+        }
+    }
+
+}
