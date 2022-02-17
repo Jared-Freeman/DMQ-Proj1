1f1e5b5693066e5413c12232d7e1d3c1f8cdc174
Evilash83 <67073295+Evilash83@users.noreply.github.com>
2022-02-16 21:19:18 -0800

    MainMenuUpdates
    Updated Main Menu scene to work with URP
    Fixed lighting issues
    Updated lighting script

--- DMQ Proj1/Assets/Scripts/MenuScripts/LightScript.cs
+++ DMQ Proj1/Assets/Scripts/MenuScripts/LightScript.cs
@@ -8,18 +8,19 @@ public class LightScript : MonoBehaviour
     public Light spotlight;
     public float baseRange = 120;
     public float baseAngle = 96;
-    public float baseIntensity = 10;
-    public float maximum = 1.0f;
-    public float minimum = -1.0f;
+    public float baseIntensity = 1000;
+    public float maximum = 10.0f;
+    public float minimum = -10.0f;
     float t = 0;
 
     // Update is called once per frame
     void Update()
     {
         t += Time.deltaTime;
-        spotlight.intensity = baseIntensity + Mathf.Lerp(minimum, maximum, t);
+        spotlight.intensity = baseIntensity + (Mathf.Lerp(minimum, maximum, t) * 10);
         spotlight.range = baseRange + (Mathf.Lerp(minimum, maximum, t) * 1);
-        spotlight.spotAngle = baseAngle + (Mathf.Lerp(minimum, maximum, t) /5);
+        spotlight.spotAngle = baseAngle + (Mathf.Lerp(minimum, maximum, t) + 40);
+        spotlight.innerSpotAngle = baseAngle + (Mathf.Lerp(minimum, maximum, t));
         if (t > 1.0)
         {
             float temp = maximum;
