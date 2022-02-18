4e883cabc3282f75cd0b460cff5e2880fbd797ad
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-02-18 12:21:49 -0800

    Added some annotations

--- DMQ Proj1/Assets/Scripts/Prototyping/Jeff Ability Prototyping/MeleeEvent.cs
+++ DMQ Proj1/Assets/Scripts/Prototyping/Jeff Ability Prototyping/MeleeEvent.cs
@@ -11,23 +11,22 @@ namespace EffectTree
         public float eventDuration = 0; //This value is set by the effect that instantiates this event.
         public List<Effect_Base> EffectList;
         private List<GameObject> targetsHit;
+
+        /// <summary>
+        /// Dispatch effect tree to targets this trigger has hit.
+        /// </summary>
+        /// <param name="col"></param>
         void OnTriggerEnter(Collider col)
         {
             if(col.gameObject.GetComponent<Actor>()) //Currently melees only register on actors. Might want to change this to hit objects.
             {
-                Team friendlyTeam = ctx.AttackData._Team;
                 Actor otherActor = col.gameObject.GetComponent<Actor>();
 
-                if(otherActor._Team != friendlyTeam) //Not sure if I need to do this. Do effects have their own team filtering?
-                {
-                   // if(!targetsHit.Contains(col.gameObject)) //Make sure we don't hit the same object twice with one melee event
-                    //{
-                        ctx.AttackData._TargetGameObject = col.gameObject; //We found an enemy actor so set it as the target.
-                        foreach (Effect_Base effect in EffectList)
-                            effect.Invoke(ref ctx);
-                   // }
-                    //targetsHit.Add(col.gameObject);
-                }
+                ctx.AttackData._TargetGameObject = col.gameObject; //We found an enemy actor so set it as the target.
+
+                foreach (Effect_Base effect in EffectList)
+                    effect.Invoke(ref ctx);
+
             }
         }
         void Update()
