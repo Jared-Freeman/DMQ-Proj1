e00b1e92e1a797488dc38524087280164eadc67e
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-02-23 12:26:17 -0800

    Move EffectContext to individual file

---
+++ DMQ Proj1/Assets/Scripts/EffectTree/EffectContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EffectTree
+{
+    /// <summary>
+    /// Any and all info needed for effect tree logic. Append more stuff here as needed. Context may be modified during effect tree invocation
+    /// </summary>
+    [System.Serializable]
+    public class EffectContext
+    {
+        /// <summary>
+        /// Attaches the supplied <paramref name="ctx"/> arg to this class
+        /// </summary>
+        /// <param name="ctx"></param>
+        public EffectContext(Utils.AttackContext ctx)
+        {
+            AttackData = ctx;
+        }
+        public EffectContext()
+        {
+            AttackData = new Utils.AttackContext();
+        }
+
+        public Utils.AttackContext AttackData = new Utils.AttackContext();
+    }
+}
