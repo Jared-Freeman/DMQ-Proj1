4c66fa8d61bc340234f503b59c0d367e37980d03
Evilash83 <67073295+Evilash83@users.noreply.github.com>
2022-02-25 15:27:16 -0800

    UI Panel updates
    Created panels for the character UI
    Created scripts for character UI
    added weapon area
    added item area
    added character class text

---
+++ DMQ Proj1/Assets/UI Components/PlayerPanel/UIPlayerPanel.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class UIPlayerPanel : MonoBehaviour
+{
+    protected SpriteRenderer _Renderer;
+
+    public GameObject activePanel;
+    public GameObject inactivePanel;
+
+    public GameObject weaponOne;
+    public GameObject weaponTwo;
+    public int activeWeapon = 0;
+
+    public GameObject[] items;
+
+    public MaterialPropertyBlock[] matProperties;
+
+    //public TextMeshProUGUI characterClassText;
+    public TMP_Text characterClassText;
+    public float time = 10.0f;
+
+    void awake()
+    {
+        _Renderer = GetComponent<SpriteRenderer>();
+        if (_Renderer == null) Destroy(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterClassText = this.GetComponentInChildren<TMP_Text>();
+        setCharacterClassName("MINE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            _Renderer.SetPropertyBlock(matProperties[i]);
+        }
+    }
+
+    #region functions
+    //function for setting character class name in UI
+    public void setCharacterClassName(string newClass)
+    {
+        characterClassText.SetText(newClass);
+    }
+
+    //Set Active weapon to front of UI
+    public void swapWeapon()
+    {
+        if(weaponOne != null && weaponTwo != null)
+        {
+            if(activeWeapon == 0)
+            {
+                activeWeapon = 1;
+                weaponOne.transform.position = new Vector3(-100,-2,-2);
+                weaponTwo.transform.position = new Vector3(-50,-1,-4);
+
+            }
+            else
+            {
+                activeWeapon = 0;
+                weaponOne.transform.position = new Vector3(-100,-2,-4);
+                weaponTwo.transform.position= new Vector3(-50,-1,-2);
+            }
+        }
+    }
+
+    //Change weapon texture, used to signal different weapons
+    public void changeWeapon(Texture tex)
+    {
+        Renderer m_renderer = null;
+        if(weaponOne != null && weaponTwo != null)
+        {
+            if(activeWeapon == 0)
+            {
+                m_renderer = weaponOne.GetComponent<Renderer>();
+                m_renderer.material.SetTexture("_WeaponTex", tex);
+            }
+            else
+            {
+                m_renderer=weaponTwo.GetComponent<Renderer>();
+                m_renderer.material.SetTexture("_WeaponTex", tex);
+            }
+        }
+    }
+
+    //Change item texture, used to signal different items
+    public void addItem(Texture tex, int index)
+    {
+        Renderer m_renderer = null;
+        m_renderer = items[index].GetComponent<Renderer>();
+        if(m_renderer != null)
+        {
+            m_renderer.material.SetTexture("_FlaskImage", tex);
+            items[index].SetActive(true);
+            matProperties[index].SetFloat("", 1);
+        }
+    }
+
+    public void addItem()
+    {
+
+    }
+
+    //Use item, decrements number of items, starts cooldown
+    public void useItem(int index)
+    {
+        float count = matProperties[index].GetFloat("_ItemCount");
+        if(count > 0)
+        {
+            count--;
+            matProperties[index].SetFloat("_ItemCount", count);
+            //StartCoroutine(itemCoolDown(time, 0));
+
+            if (count == 0)
+            {
+                items[index].SetActive(false);
+            }
+        }
+    }
+
+
+    #endregion
+}
---
+++ DMQ Proj1/Assets/UIItemScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIItemScript : MonoBehaviour
+{
+    public GameObject item;
+    public Texture2D testTexture;
+    public new SpriteRenderer renderer;
+    public int itemCount = 2;
+    public bool onCoolDown = false;
+    public MaterialPropertyBlock matProperties;
+
+
+    void Awake()
+    {
+        item = this.gameObject;
+        renderer = GetComponent<SpriteRenderer>();
+        if (renderer == null) Destroy(this);
+        matProperties = new MaterialPropertyBlock();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //renderer = GetComponent<Renderer>();
+        useItem(4.0f);
+        changeItem(testTexture);
+
+    }
+
+    void update()
+    {
+        renderer.SetPropertyBlock(matProperties);
+    }
+
+    public void changeItem(Texture2D tex)
+    {
+        renderer.material.SetTexture("_FlaskImage",tex);
+    }
+
+    public void updateItemCount()
+    {
+        itemCount++;
+    }
+
+    public void useItem(float coolDownTime)
+    {
+        Debug.Log("got here too");
+        if(itemCount > 0 && !onCoolDown)
+        {
+            Debug.Log("got here");
+            onCoolDown = true;
+            matProperties.SetFloat("_Timer", 1.0f);
+            StartCoroutine(itemCoolDown(coolDownTime));
+            itemCount--;
+            if(itemCount == 0)
+            {
+                changeItem(testTexture);
+            }
+            onCoolDown = false;
+        }
+    }
+
+    //Coroutine to control cooldown on texture
+    private IEnumerator itemCoolDown(float itemCoolDownTime)
+    {
+        Debug.Log("This worked");
+        float totalCoolDownTime = itemCoolDownTime;
+        while (itemCoolDownTime > 0)
+        {
+            Debug.Log("Counting Down");
+            itemCoolDownTime -= Time.deltaTime;
+            matProperties.SetFloat("_Timer", itemCoolDownTime / totalCoolDownTime);
+            renderer.SetPropertyBlock(matProperties);
+            yield return null;
+        }
+        Debug.Log("Finished");
+    }
+}
