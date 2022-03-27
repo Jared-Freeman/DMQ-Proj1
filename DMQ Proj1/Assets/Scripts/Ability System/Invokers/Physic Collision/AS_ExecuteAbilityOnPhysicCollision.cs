9b696fb658974ca23269ee0b294103001148c0fa
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-27 14:16:01 -0700

    Ability invoke on Physic Collision
    A component. Useful for placing on a Prop (Will eventually be added to Prop functionality as a requiredcomponent). May get a SO preset in the future.

---
+++ DMQ Proj1/Assets/Scripts/Ability System/Invokers/Physic Collision/AS_ExecuteAbilityOnPhysicCollision.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbilitySystem
+{
+    /// <summary>
+    /// Attempts to cast the supplied ability when the attached actor Stats takes damage.
+    /// </summary>
+    public class AS_ExecuteAbilityOnPhysicCollision : MonoBehaviour
+    {
+        #region Members
+
+        //presets
+        /// <summary>
+        /// Ability to invoke upon CollisionEnter Unity message
+        /// </summary>
+        public AbilitySystem.AS_Ability_Base Ability_CollisionEnter;
+        /// <summary>
+        /// Ability to invoke upon CollisionStay Unity message
+        /// </summary>
+        /// <remarks>
+        /// Recall that abilities have a cooldown. Using a short cooldown, CollisionStay can be useful for having sparks fly when we're scraping against metal
+        /// </remarks>
+        public AbilitySystem.AS_Ability_Base Ability_CollisionStay;
+        /// <summary>
+        /// Ability to invoke upon CollisionExit Unity message
+        /// </summary>
+        public AbilitySystem.AS_Ability_Base Ability_CollisionExit;
+
+        //instances
+        protected AbilitySystem.AS_Ability_Instance_Base _AbilityInstanceEnter;
+        protected AbilitySystem.AS_Ability_Instance_Base _AbilityInstanceStay;
+        protected AbilitySystem.AS_Ability_Instance_Base _AbilityInstanceExit;
+
+        #region Properties
+
+        //ability instance property public-accessors
+        public AbilitySystem.AS_Ability_Instance_Base AbilityInstanceEnter { get => _AbilityInstanceEnter; }
+        public AbilitySystem.AS_Ability_Instance_Base AbilityInstanceStay { get => _AbilityInstanceStay; }
+        public AbilitySystem.AS_Ability_Instance_Base AbilityInstanceExit { get => _AbilityInstanceExit; }
+
+        #endregion
+
+        #endregion
+
+        void Start()
+        {
+            _AbilityInstanceEnter = Ability_CollisionEnter.GetInstance(gameObject);
+            _AbilityInstanceStay = Ability_CollisionStay.GetInstance(gameObject);
+            _AbilityInstanceExit = Ability_CollisionExit.GetInstance(gameObject);
+        }
+
+        void OnCollisionEnter(Collision c)
+        {
+            EffectTree.EffectContext ctx = new EffectTree.EffectContext();
+            ctx.ContextData = EffectTree.EffectContext.CreateContextDataFromCollision(c);
+            ctx.AttackData = EffectTree.EffectContext.CreateAttackContextDataFromCollision(c);
+
+            _AbilityInstanceEnter?.ExecuteAbility(ref ctx);
+        }
+        void OnCollisionStay(Collision c)
+        {
+            EffectTree.EffectContext ctx = new EffectTree.EffectContext();
+            ctx.ContextData = EffectTree.EffectContext.CreateContextDataFromCollision(c);
+            ctx.AttackData = EffectTree.EffectContext.CreateAttackContextDataFromCollision(c);
+
+            AbilityInstanceStay?.ExecuteAbility(ref ctx);
+        }
+        void OnCollisionExit(Collision c)
+        {
+            EffectTree.EffectContext ctx = new EffectTree.EffectContext();
+            ctx.ContextData = EffectTree.EffectContext.CreateContextDataFromCollision(c);
+            ctx.AttackData = EffectTree.EffectContext.CreateAttackContextDataFromCollision(c);
+
+            AbilityInstanceExit?.ExecuteAbility(ref ctx);
+        }
+    }
+}
