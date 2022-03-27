cde2842918ba211f89ca8dbed78059ca1c990325
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-27 14:36:06 -0700

    Prop rudiments
    preset architecture + ref validations

--- DMQ Proj1/Assets/Scripts/Prop System/PS_Prop_Base.cs
+++ DMQ Proj1/Assets/Scripts/Prop System/PS_Prop_Base.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_Prop_Base : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using AbilitySystem;
 
-    // Update is called once per frame
-    void Update()
+namespace PropSystem
+{
+
+    /// <summary>
+    /// Base class for Props. 
+    /// </summary>
+    /// <remarks>This component gives prop items additional functionality such as Abilities upon collision.</remarks>
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(AbilitySystem.AS_ExecuteAbilityOnPhysicCollision))]
+    public class PS_Prop_Base : MonoBehaviour
     {
-        
+        #region Members
+
+        protected AS_ExecuteAbilityOnPhysicCollision _CollisionInvoker;
+        protected Rigidbody _RB;
+
+        #endregion
+
+        #region Initialization
+
+        void Awake()
+        {
+            _CollisionInvoker = GetComponent<AS_ExecuteAbilityOnPhysicCollision>();
+            if (!Utils.Testing.ReferenceIsValid(_CollisionInvoker)) Destroy(this);
+
+            _RB = GetComponent<Rigidbody>();
+            if (!Utils.Testing.ReferenceIsValid(_RB)) Destroy(this);
+        }
+
+        #endregion
     }
 }
