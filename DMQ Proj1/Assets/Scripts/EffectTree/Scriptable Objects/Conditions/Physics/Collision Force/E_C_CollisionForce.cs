d1edd1ec9babc14fb748665336be3713c1d7de98
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-28 17:26:15 -0700

    Collision Force EffectTree condition rudiments

---
+++ DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Conditions/Physics/Collision Force/E_C_CollisionForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EffectTree.Condition
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(fileName = "Cond_PC_", menuName = "Effect Tree/Conditions/Collision Force", order = 5)]
+    public class E_C_CollisionForce : E_Condition_Base
+    {
+        public override bool EvaluateCondition(ref EffectContext ctx)
+        {
+            if(base.EvaluateCondition(ref ctx))
+            {
+
+
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
