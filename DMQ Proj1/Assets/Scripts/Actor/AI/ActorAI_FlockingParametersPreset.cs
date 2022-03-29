b1824abd107743c8eec2c6fa39bea013b304533d
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-29 15:48:35 -0700

    Flocking rudiments
    Parameters SO and icon. Next step is adding a utility method in Actor AI that uses the Flocking SO to provide locality telemetry to the Logic instance

---
+++ DMQ Proj1/Assets/Scripts/Actor/AI/ActorAI_FlockingParametersPreset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ActorSystem.AI.Flocking
+{
+    public class ActorAI_FlockingParametersPreset : ScriptableObject
+    {
+
+    }
+}
