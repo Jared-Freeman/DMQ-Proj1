7f81eeaee2183ed1a9cb9df3e95ffd2dd7de9b9b
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-28 17:44:22 -0700

    Collision Exists impl. Collision force rudiments

---
+++ DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Conditions/Physics/Collision Exists/E_C_CollisionExists.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace EffectTree.Condition
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(fileName = "Cond_PC_", menuName = "Effect Tree/Conditions/Collision Force", order = 2)]
+    public class E_C_CollisionExists : E_Condition_Base
+    {
+        public E_C_ColExistsOptions AllowedCollisions = E_C_ColExistsOptions.CollisionsAllowed;
+
+        public enum E_C_ColExistsOptions { TriggersAllowed, CollisionsAllowed, TriggersAndCollisionAllowed }
+
+        /// <summary>
+        /// Public-facing version of this Condition eval. 
+        /// </summary>
+        /// <remarks>
+        /// Useful for a few other conditions to reuse this logic. See <see cref="E_C_CollisionForce"/>.
+        /// </remarks>
+        public static bool CollisionExists(ref EffectContext ctx)
+        {
+            if (ctx.ContextData._TriggeringCollision != null) return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Public-facing version of this Condition eval. 
+        /// </summary>
+        /// <remarks>
+        /// Useful for a few other conditions to reuse this logic.
+        /// </remarks>
+        public static bool TriggerExists(ref EffectContext ctx)
+        {
+            if (ctx.ContextData._TriggeringCollider != null && ctx.ContextData._TriggeringCollision == null) return true;
+
+            return false;
+        }
+
+        public override bool EvaluateCondition(ref EffectContext ctx)
+        {
+            if( base.EvaluateCondition(ref ctx))
+            {
+                switch(AllowedCollisions)
+                {
+
+                    case E_C_ColExistsOptions.CollisionsAllowed:
+                        if (CollisionExists(ref ctx)) return true;
+                        break;
+
+                    case E_C_ColExistsOptions.TriggersAllowed:
+                        if (TriggerExists(ref ctx)) return true;
+                        break;
+
+                    case E_C_ColExistsOptions.TriggersAndCollisionAllowed:
+                        if (CollisionExists(ref ctx) || TriggerExists(ref ctx)) return true;
+                        break;
+
+                    default:
+                        Debug.LogError("No impl found for condition option!");
+                        return false;   
+
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
--- DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Conditions/Physics/Collision Force/E_C_CollisionForce.cs
+++ DMQ Proj1/Assets/Scripts/EffectTree/Scriptable Objects/Conditions/Physics/Collision Force/E_C_CollisionForce.cs
@@ -7,17 +7,32 @@ namespace EffectTree.Condition
     /// <summary>
     /// 
     /// </summary>
-    [CreateAssetMenu(fileName = "Cond_PC_", menuName = "Effect Tree/Conditions/Collision Force", order = 5)]
+    [CreateAssetMenu(fileName = "Cond_PC_", menuName = "Effect Tree/Conditions/Collision Force", order = 2)]
     public class E_C_CollisionForce : E_Condition_Base
     {
+        /// <summary>
+        /// Controls how we interpret force data from this collision
+        /// </summary>
+        public E_C_CollisionForceOptions CollisionInterpreterStyle = E_C_CollisionForceOptions.UseFirstContactPoint;
+        /// <summary>
+        /// We compare the sqr magnitude of contact force to this amount^2
+        /// </summary>
+        public float ForceAmount;
+
+
+        public enum E_C_CollisionForceOptions { UseFirstContactPoint, UseAverageImpulse, UseHighestImpulse, UseLowestImpulse };
+
+
         public override bool EvaluateCondition(ref EffectContext ctx)
         {
             if(base.EvaluateCondition(ref ctx))
             {
+                if(E_C_CollisionExists.CollisionExists(ref ctx))
+                {
 
 
-
-                return true;
+                    return true;
+                }
             }
             return false;
         }
