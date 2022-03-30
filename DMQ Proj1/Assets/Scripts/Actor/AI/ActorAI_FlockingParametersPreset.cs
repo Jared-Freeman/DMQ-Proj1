224b0e181fbb5a915882182729180581d2feef93
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-03-30 10:32:47 -0700

    anno

--- DMQ Proj1/Assets/Scripts/Actor/AI/ActorAI_FlockingParametersPreset.cs
+++ DMQ Proj1/Assets/Scripts/Actor/AI/ActorAI_FlockingParametersPreset.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 namespace ActorSystem.AI.Flocking
 {
+    /// <summary>
+    /// Container for Flocking Parameters for use with <see cref="ActorAI_Logic"/> 
+    /// </summary>
+    /// <remarks>
+    /// Flocking is an AI formation behavior algorithm popularized by the Boids simulation originally developed by Craig Reynolds
+    /// <para>Some possibly useful reading:</para>
+    /// <para>https://www.oreilly.com/library/view/ai-for-game/0596005555/ch04.html</para>
+    /// <para>https://cs.stanford.edu/people/eroberts/courses/soco/projects/2008-09/modeling-natural-systems/boids.html</para>
+    /// </remarks>
     public class ActorAI_FlockingParametersPreset : ScriptableObject
     {
 
