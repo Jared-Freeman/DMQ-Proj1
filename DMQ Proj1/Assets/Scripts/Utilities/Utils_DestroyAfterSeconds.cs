3b2b972ac0683d227145f16a780ef8581062231d
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-04-03 04:25:24 -0700

    ragdoll destroy after short duration

---
+++ DMQ Proj1/Assets/Scripts/Utilities/Utils_DestroyAfterSeconds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This component simply destroys the object it is attached to after the specified duration.
+/// </summary>
+/// <remarks>
+/// Useful for ragdolls and other debris that should be cleaned up
+/// </remarks>
+public class Utils_DestroyAfterSeconds : MonoBehaviour
+{
+    public float Duration;
+
+    void Start()
+    {
+        StartCoroutine(DestroyAfterSeconds(Duration));
+    }
+
+    protected IEnumerator DestroyAfterSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Destroy(gameObject);
+    }
+}
