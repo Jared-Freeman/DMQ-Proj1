ef84b2ad03b6b64c55a043cc1526cac733340fce
Rycorn <44075236+Rycorn@users.noreply.github.com>
2022-04-19 19:48:47 -0700

    Game over logic update
    no longer tries to reload scene every frame and changes back to main menu after timer

--- DMQ Proj1/Assets/Scripts/GameOverLogic.cs
+++ DMQ Proj1/Assets/Scripts/GameOverLogic.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverLogic : MonoBehaviour
 {
-    public Topdown_Multitracking_Camera_Rig camera_Rig;
+    public TMP_Text timerText;
+    private float timer = 10.5f;
 
     bool playerWasSpawned = false;
 
     public string sceneToLoad;
+    public string mainMenuSceneToLoad;
 
     // Update is called once per frame
     void Update()
     {
-        if (!playerWasSpawned && camera_Rig.TargetList.Count > 0)
-            playerWasSpawned = true;
-
-        else if(playerWasSpawned && camera_Rig.TargetList.Count == 0)
+        if (SceneManager.GetActiveScene().name == sceneToLoad)
         {
-            if(sceneToLoad != null)
-                SceneManager.LoadScene(sceneToLoad);
+            timerText.gameObject.SetActive(true);
+            timer -= Time.deltaTime;
+            timer = Mathf.Max(timer, 0);
+            timerText.text = "Return to Main Menu\n" + Mathf.FloorToInt(timer).ToString();
+            if(timer <= 0)
+            {
+                SceneManager.LoadScene(mainMenuSceneToLoad);
+
+            }
+        }
+        else
+        {
+
+            if (!playerWasSpawned && Singleton<Topdown_Multitracking_Camera_Rig>.Instance.TargetList.Count > 0)
+                playerWasSpawned = true;
+
+            else if (playerWasSpawned && Singleton<Topdown_Multitracking_Camera_Rig>.Instance.TargetList.Count == 0)
+            {
+                Debug.Log("Game Over");
+                if (sceneToLoad != null)
+                    SceneManager.LoadScene(sceneToLoad);
+            }
         }
     }
 }
