8a2c14d3dfc8d76302f6f8d01bf6a9ac5461e816
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-04-19 19:50:14 -0700

    intro anim + rsc + init logic

---
+++ DMQ Proj1/Assets/Scripts/Level Transition/LT_DungeonLevelInitialization.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelTransition
+{
+    public class LT_DungeonLevelInitialization : MonoBehaviour
+    {
+        void Start()
+        {
+            StartCoroutine(InitializeElements());
+        }
+
+        /// <summary>
+        /// Initializes a dungeon level
+        /// </summary>
+        public IEnumerator InitializeElements()
+        {
+            //wait a few frames for init computations
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+
+            //ensure camera is in right place
+            Singleton<Topdown_Multitracking_Camera_Rig>.Instance.WarpToCurrentDesiredLocation();
+            //ensure game state is Gameplay
+            Singleton<GameState.GameStateManager>.Instance.InvokeResume();
+        }
+    }
+}
