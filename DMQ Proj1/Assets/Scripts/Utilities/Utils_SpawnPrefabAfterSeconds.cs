6cc08faa3bb565b6a7d55229bbefe334294e40cb
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-04-20 09:15:42 -0700

    Pause Menu

---
+++ DMQ Proj1/Assets/Scripts/Utilities/Utils_SpawnPrefabAfterSeconds.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public class Utils_SpawnPrefabAfterSeconds : MonoBehaviour
+    {
+        public float Seconds = 0f;
+
+        public bool UseUnscaledTime = true;
+
+        public GameObject Prefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Prefab == null) Destroy(this);
+
+            if(UseUnscaledTime)
+            {
+                StartCoroutine(I_ContinueDelayUnscaled(Seconds));
+            }
+            else
+            {
+                StartCoroutine(I_ContinueDelay(Seconds));
+            }
+        }
+
+
+        protected IEnumerator I_ContinueDelayUnscaled(float duration)
+        {
+            float startTime = Time.unscaledTime;
+            while (Mathf.Abs(Time.unscaledTime - startTime) < duration)
+            {
+                yield return null;
+            }
+
+            var g = Instantiate(Prefab);
+
+            g.transform.position = transform.position;
+
+            Destroy(this);
+        }
+
+
+        protected IEnumerator I_ContinueDelay(float duration)
+        {
+            float startTime = Time.time;
+            while (Mathf.Abs(Time.time - startTime) < duration)
+            {
+                yield return null;
+            }
+
+            var g = Instantiate(Prefab);
+
+            g.transform.position = transform.position;
+
+            Destroy(this);
+        }
+    }
+
+
+}
