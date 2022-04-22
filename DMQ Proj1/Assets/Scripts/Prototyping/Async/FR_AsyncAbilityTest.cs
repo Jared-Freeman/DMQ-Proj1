cd952e1a48c1cdc5f20a87e84d2cf4950b63842e
Jared-Freeman <48396677+Jared-Freeman@users.noreply.github.com>
2022-04-22 11:21:12 -0700

    org + async stuff
    - test async scene
    - update CooldownTracker to use cancellation token for early disposal of rsc
    - staging ground for more important AI async impl

---
+++ DMQ Proj1/Assets/Scripts/Prototyping/Async/FR_AsyncAbilityTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Init an ability then kill it early to see if syst is working
+/// </summary>
+public class FR_AsyncAbilityTest : MonoBehaviour
+{
+    AbilitySystem.AS_Ability_Instance_Base inst;
+
+    void Start()
+    {
+        StartCoroutine(I_LateGetInst());
+    }
+    void OnDestroy()
+    {
+        inst.Cooldown.OnCooldownUsed -= Cooldown_OnCooldownUsed;
+    }
+
+    //cast on first frame we can
+    void Update()
+    {
+        if(!inst) { return; }
+
+
+        if (inst.CanCastAbility)
+        {
+            print("inst ability cast");
+            var c = new EffectTree.EffectContext()
+            {
+                AttackData = new Utils.AttackContext(),
+                ContextData = new EffectTree.EffectContext.EffectContextInfo()
+            };
+            inst.ExecuteAbility(ref c);
+        }
+    }
+
+    private void Cooldown_OnCooldownUsed(object sender, Utils.CooldownTracker.CooldownTrackerEventArgs e)
+    {
+        print("starting early destroy");
+
+        StartCoroutine(I_EarlyKill());
+    }
+
+    private IEnumerator I_EarlyKill()
+    {
+        yield return null;
+        yield return null;
+        yield return null;
+
+        print("early destroy");
+        Destroy(inst);
+    }
+    private IEnumerator I_LateGetInst()
+    {
+        yield return null;
+        yield return null;
+        yield return null;
+
+        inst = GetComponent<AbilitySystem.AS_Ability_Instance_Base>();
+
+        if (!inst) Destroy(this);
+        else print("inst acquired");
+
+        inst.Cooldown.OnCooldownUsed += Cooldown_OnCooldownUsed;
+    }
+}
